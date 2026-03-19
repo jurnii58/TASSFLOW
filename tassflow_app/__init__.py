9559e5c14edb56399b253c73d8d2d7385fb05048
Luis Antonio Fernandez <antua58a13@gmail.com>
2026-03-18 19:17:12 -0600

    Update __init__.py

--- tassflow_app/__init__.py
+++ tassflow_app/__init__.py
@@ -44,4 +44,10 @@ def create_app(config_class=Config):
     app.register_blueprint(usuario_bp)
     app.register_blueprint(chat_bp)
 
+    @app.after_request
+    def add_header(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+        return response
     return app
